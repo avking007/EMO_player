299a32d35e142b4167597391f80f4e0dfd0fd2dc
meghnavarma0 <meghnavarma0@gmail.com>
2021-04-18 18:48:01 +0530

    added header component

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import SignIn from "./Compenents/Auth/SignIn/SignIn";
 import SignUp from "./Compenents/Auth/SignUp/SignUp";
-import UserDashboard from "./Compenents/Layout/UserDashboard";
+import SimpleAppBar from "./Compenents/Layout/SimpleAppBar";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <SimpleAppBar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={SignIn} />
         <Route path="/signup" exact component={SignUp} />
-        <Route path="/user/dashboard" exact component={UserDashboard} />
+        {/* <Route path="/user/dashboard" exact component={SimpleAppBar} /> */}
       </Switch>
     </BrowserRouter>
   );
