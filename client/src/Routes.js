3567b55aff41cc33f0e58bf83308a0315f97bea5
Anish Varshney <avanvarshney@gmail.com>
2021-05-02 16:23:47 +0530

    Merged master into Anish

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,21 +3,25 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Components/layout/Home";
 import SignIn from "./Components/auth/SignIn/SignIn";
 import SignUp from "./Components/auth/SignUp/SignUp";
-import UserDashboard from "./Components/layout/UserDashboard";
+import SimpleAppBar from "./Components/layout/SimpleAppBar";
+import SwipeMenu from "./Components/layout/SwipeMenu";
+import CurrentSection from "./Components/CurrentSection";
 import MoodDetector from "./Components/MoodDetector/MoodDetector";
 
 const Routes = () => {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/signin" exact component={SignIn} />
-          <Route path="/signup" exact component={SignUp} />
-          <Route path="/user/dashboard" exact component={UserDashboard} />
-          <Route path="/mood" component={MoodDetector} exact/>
-        </Switch>
+  return (
+    <BrowserRouter>
+      <SimpleAppBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/home" exact component={CurrentSection} />
+        <Route path="/mood" component={MoodDetector} exact/>
+      </Switch>
+      <SwipeMenu />
     </BrowserRouter>
-    );
-  };
-  
-  export default Routes;
+  );
+};
+
+export default Routes;
---
+++ client/src/apis/suggestSearch.js
@@ -0,0 +1,9 @@
+const suggestSearch = {
+  baseURL: "https://suggestqueries.google.com/complete/search?",
+  params: {
+    hl: "en", // Language
+    ds: "yt", // Restrict lookup to youtube
+    client: "youtube", // force youtube style response, i.e. jsonp
+  },
+};
+export default suggestSearch;
