2d29edca3a2ffd5827514221a24ab26561621b3e
Anish <avanvarshney@gmail.com>
2021-06-20 21:27:33 +0530

    Created action types

---
+++ client/src/reducers/types.js
@@ -0,0 +1,9 @@
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAIL = 'SIGNUP_FAIL';
+
+export const USER_LOADED = 'USER_LOADED';
+
