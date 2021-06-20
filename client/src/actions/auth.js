40f9d25cb2a06ea6fbaccda87d5261f7ebc1e930
Anish <avanvarshney@gmail.com>
2021-06-20 21:27:57 +0530

    Created auth actions

---
+++ client/src/actions/auth.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, SIGNUP_FAIL, SIGNUP_SUCCESS, USER_LOADED } from '../reducers/types';
+import { API } from '../utils/backend';
+import setAuthToken from '../utils/setAuthToken';
+
+export const loadUser = () => async(dispatch) => {
+
+    if (localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const user = await axios.get(`${API}/user`);
+        dispatch({type: USER_LOADED, payload: user.data});
+    } catch (error) {
+        console.log(error);
+        dispatch({type: AUTH_ERROR});
+    }
+}
+
+export const login = (userDetails) => async(dispatch) => {
+    try {
+        const { email, password } = userDetails;
+        const config = {
+            'Content-Type': 'application/json'
+        };
+        const body = JSON.stringify({email, password});
+
+        const user = await axios.post(`${API}/user/login`, body, config);
+
+        console.log(user);
+        dispatch({type: LOGIN_SUCCESS, payload: user.data});
+    } catch (error) {
+        console.log(error);
+        dispatch({type: LOGIN_FAIL});
+    }
+};
+
+export const signUp = (userDetails) => async(dispatch) => {
+    try {
+        const { email, password } = userDetails;
+        const config = {
+            'Content-Type': 'application/json'
+        };
+        const body = JSON.stringify({email, password});
+
+        const user = await axios.post(`${API}/user/signup`, body, config);
+
+        console.log(user);
+        dispatch({type: SIGNUP_SUCCESS, payload: user.data});
+    } catch (error) {
+        dispatch({type: SIGNUP_FAIL });
+        console.log(error);
+    }
+};
