d4332aaff0982d246a16bdfd3b067d00216e76b5
Anish <avanvarshney@gmail.com>
2021-06-21 11:24:20 +0530

    Load user on app load

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,16 @@ import Routes from "./Routes";
 import { Provider } from "react-redux";
 import { GlobalState } from "./Components/GlobalState";
 import store from './store';
+import { useEffect } from "react";
+import { loadUser } from "./actions/auth";
 
 const App = () => {
+  
+  // load user on app load if token present
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, [])
+
   return (
     <Provider store={store}>
       <GlobalState>
