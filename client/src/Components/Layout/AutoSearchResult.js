c0921b0c46152265761f435393898d0453789559
Anish <avanvarshney@gmail.com>
2021-06-21 11:32:04 +0530

    Removed redundant file

--- client/src/Components/Layout/AutoSearchResult.js
+++ client/src/Components/Layout/AutoSearchResult.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Base from "../../core/Base";
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
 
@@ -8,18 +7,16 @@ const AutoSearchResult = ({ results, onSearchSelect }) => {
   if (results) {
     renderResult = results.map((result, index) => {
       return (
-        <Base>
-          <ListItem
-            key={index}
-            onClick={() => onSearchSelect(result[0])}
-            button
-          >
-            <ListItemIcon style={{ paddingLeft: "16px" }}>
-              <Search />
-            </ListItemIcon>
-            <ListItemText primary={result[0]} />
-          </ListItem>
-        </Base>
+        <ListItem
+          key={index}
+          onClick={() => onSearchSelect(result[0])}
+          button
+        >
+          <ListItemIcon style={{ paddingLeft: "16px" }}>
+            <Search />
+          </ListItemIcon>
+          <ListItemText primary={result[0]} />
+        </ListItem>
       );
     });
   }
