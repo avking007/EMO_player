16280aa1d37a0f6b95e5825d77f61622c70f82e1
Anish <avanvarshney@gmail.com>
2021-06-21 15:04:45 +0530

    Created bottom menu tabs

---
+++ client/src/Components/Layout/BottomMenu.js
@@ -0,0 +1,95 @@
+import { Tab, Tabs, withStyles } from '@material-ui/core';
+import { Home, Favorite, GetApp, History } from '@material-ui/icons';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const BottomMenu = () => {
+    
+    const CustomTabs = withStyles({
+        root: {
+            background: "#191414",
+            position: "fixed",
+            bottom: "0",
+            padding: 0,
+            width: "100%",
+            zIndex: 1300,
+        },
+        indicator: {
+            display: "none",
+        },
+        labelIcon: {
+            margin: 0,
+        },
+    })(Tabs);
+
+    const CustomTab = withStyles({
+        root: {
+            color: "#1db954",
+            fontWeight: "bold",
+            fontSize: ".75rem",
+            margin: 0,
+
+            "&:hover": {
+                color: "#e4e6eb",
+                textDecoration: "none",
+                opacity: 1,
+            },
+            "&$selected": {
+                color: "#b3b3b3",
+            },
+            "&:focus": {
+                color: "#b3b3b3",
+            },
+        },
+
+        selected: {},
+    })(Tab);
+    
+    const [tabValue, setTabValue] = useState(0);
+
+    const handleChange = (event, value) => {
+        setTabValue(value);
+    }
+    return (
+        <CustomTabs
+            value={tabValue}
+            onChange={handleChange}
+            variant="fullWidth"
+            indicatorColor="primary"
+            textColor="primary"
+        >
+            <CustomTab
+                icon={<Home />}
+                aria-label="Home"
+                component={Link}
+                to="/home"
+                label="Home"
+            />
+
+            <CustomTab
+                icon={<Favorite />}
+                aria-label="Liked"
+                component={Link}
+                to="/liked"
+                label="Liked"
+            />
+
+            <CustomTab
+                icon={<GetApp />}
+                aria-label="Downloads"
+                component={Link}
+                to="/downloads"
+                label="Downloads"
+            />
+            <CustomTab
+                icon={<History />}
+                aria-label="History"
+                component={Link}
+                to="/history"
+                label="History"
+            />
+        </CustomTabs>
+    );
+};
+
+export default BottomMenu
