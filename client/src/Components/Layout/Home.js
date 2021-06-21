e785c9365f069de99e0901cde39a08d3faf4d120
Anish <avanvarshney@gmail.com>
2021-06-21 08:47:19 +0530

    Removed reducndant component

--- client/src/Components/Layout/Home.js
+++ client/src/Components/Layout/Home.js
@@ -1,25 +1,27 @@
-import Menu  from "../../core/Menu";
+import Menu from "../../core/Menu";
 import React from "react";
 import "../../App.css";
-import Base from "../../core/Base";
 
 const Home = () => {
   return (
-    <Base>
-      <div className="Block_one">
-        <h1>
-          <span className="Main"> EMO </span>
-          <br></br>Welcomes you to it's world..
-        </h1>
-        <p>
-          {" "}
-          (An application that suggest songs based on your mood by capturing
-          facial expressions.)
-        </p>
-        <span className="text-muted">An amazing site for music fanatics..</span>
+    <div className="container-fluid p-4">
+      <div className="bg-dark text-white p-4">
+
+        <div className="Block_one">
+          <h1>
+            <span className="Main"> EMO </span>
+            <br></br>Welcomes you to it's world..
+          </h1>
+          <p>
+            {" "}
+            (An application that suggest songs based on your mood by capturing
+            facial expressions.)
+          </p>
+          <span className="text-muted">An amazing site for music fanatics..</span>
+        </div>
       </div>
       <Menu />
-    </Base>
+    </div>
   );
 };
 
