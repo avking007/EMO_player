ff5650e0a825bceef3790c6f4dad199c9bb226fb
Anish <avanvarshney@gmail.com>
2021-07-04 16:57:51 +0530

    Fixed exhaustive component rerender

--- client/src/Components/MoodDetector/MoodPlaylist/ListItem.js
+++ client/src/Components/MoodDetector/MoodPlaylist/ListItem.js
@@ -1,102 +1,39 @@
 import {
   Avatar,
-  CircularProgress,
   Divider,
-  Grid,
   ListItemAvatar,
   ListItemText,
 } from "@material-ui/core";
-import { motion } from "framer-motion";
-import React, { useState } from "react";
-import { useEffect } from "react";
-import youtubeSearch from "../../../apis/youtubeSearch";
+import React from "react";
 
 const Entities = require("html-entities").XmlEntities;
 const entities = new Entities();
 
-const ListItem = ({ song }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentVideoSnippet, setCurrentVideoSnippet] = useState({});
-  const liVariants = {
-    open: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        y: { stiffness: 1000, velocity: -100 },
-      },
-    },
-    closed: {
-      y: 50,
-      opacity: 0,
-      transition: {
-        y: { stiffness: 1000 },
-      },
-    },
-  };
-  const CircularLoader = (
-    <Grid
-      style={{ height: "109vh" }}
-      container
-      justify="center"
-      alignItems="center"
-    >
-      <CircularProgress />
-    </Grid>
-  );
-  const fetchAndSetCurrentVideoSnippet = async (id) => {
-    try {
-      const res = await youtubeSearch.get("videos", { params: { id: id } });
-      const item = res.data.items[0];
-
-      setCurrentVideoSnippet({
-        id: item.id,
-        title: item.snippet.title,
-        channelTitle: item.snippet.channelTitle,
-        maxThumbnail: `https://img.youtube.com/vi/${item.id}/maxresdefault.jpg`,
-        sdThumbnail: `https://img.youtube.com/vi/${item.id}/sddefault.jpg`,
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchAndSetCurrentVideoSnippet(song.songId);
-  }, [song.songId]);
-
-  return isLoading ? (
-    <CircularLoader />
-  ) : (
-    <motion.div variants={liVariants} key={song.songId}>
-      <ListItem
-        alignItems="flex-start"
-        button
-        onClick={() => {}}
-        style={{ color: "#fff" }}
-      >
+const ListItem = ({song}) => {
+  return (
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center', color: '#fff', padding: '1rem', justifyContent:'center'}}>
         <ListItemAvatar>
           <Avatar
             className="searchThumb"
             style={{ width: "60px", height: "60px", marginRight: "15px" }}
-            alt={currentVideoSnippet.title}
-            src={currentVideoSnippet.thumbnails.high.url}
+            alt={song?.title}
+            src={song?.thumbnail}
           />
         </ListItemAvatar>
         <ListItemText
-          primary={entities.decode(currentVideoSnippet.title, { level: "xml" })}
+          primary={entities.decode(song?.title, { level: "xml" })}
           secondary={
             <>
               <span style={{ color: "#fff" }}>
-                {currentVideoSnippet.channelTitle}
+                {song?.channelTitle}
               </span>
             </>
           }
         />
-      </ListItem>
-      <Divider />
-    </motion.div>
+      </div>
+      <Divider style={{background:'#fff'}} />
+    </div>
   );
 };
 
