f3e4a740653be68a984c60aefdce2cbe94f9775d
Anish <avanvarshney@gmail.com>
2021-07-04 16:57:11 +0530

    Fixed app crash

--- client/src/Components/MoodDetector/MoodPlaylist/MoodPlaylist.js
+++ client/src/Components/MoodDetector/MoodPlaylist/MoodPlaylist.js
@@ -1,32 +1,19 @@
 import { CircularProgress, Grid } from "@material-ui/core";
-import { motion, useCycle } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
 import { userPlaySong, userSkipSong } from "../../../actions/songs";
 import youtubeSearch from "../../../apis/youtubeSearch";
 import { emoAlgorithm } from "../../../utils/emoAlgorithm";
 import FetchPlaylist from "./FetchPlaylist";
 import ListItem from "./ListItem";
 
-const ulVariants = {
-  open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-  },
-  closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
-  },
-};
-
-// ComponentDidMount
-const MoodPlaylist = ({ songsArray, userPlaySong, userSkipSong, match }) => {
+const MoodPlaylist = ({ areUserSongsLoading, songsArray, userPlaySong, userSkipSong, match }) => {
   const [searchResult, setSearchResult] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isOpen, setisOpen] = useCycle(false, true);
 
   const { mood } = match.params;
 
-  const CircularLoader = (
+  const CircularLoader = () => (
     <Grid
       style={{ height: "109vh" }}
       container
@@ -37,60 +24,73 @@ const MoodPlaylist = ({ songsArray, userPlaySong, userSkipSong, match }) => {
     </Grid>
   );
 
-  useEffect(() => {
-    const searchYt = async (data) => {
-      const res = await youtubeSearch.get("/search", {
-        params: {
-          q: data,
-          maxResults: 15 - songsArray.mood.length,
-        },
-      });
-      setSearchResult(res.data.items);
-      setIsLoading(false);
-    };
-
-    if (songsArray.mood.length < 15) {
-      searchYt(`${mood} songs`);
-    }
-  }, [mood, songsArray]);
-  useEffect(() => {
-    const newPlayList = emoAlgorithm(songsArray);
-    setSearchResult([...newPlayList]);
-  }, [songsArray]);
-
-  useEffect(() => {
-    setisOpen(true);
-  }, [setisOpen]);
-
   const getCustomPlaylist = (songs) => {
     const newPlaylist = emoAlgorithm(songs);
+
     return (
-      <motion.div
-        variants={ulVariants}
-        initial={false}
-        animate={isOpen ? "open" : "closed"}
+      <div
+      // variants={ulVariants}
+      // initial={false}
+      // animate={isOpen ? "open" : "closed"}
       >
         {newPlaylist.map((song, index) => {
           return <ListItem key={index} song={song} />;
         })}
-      </motion.div>
+      </div>
     );
   };
-  return isLoading ? (
-    <CircularLoader />
-  ) : (
-    <div>
-      {songsArray.mood.length && getCustomPlaylist(songsArray.mood)}
 
-      <FetchPlaylist videos={searchResult} />
-    </div>
-  );
-};
+  useEffect(() => {
+    const getSearchResults = async () => {
+      try {
+        const res = await youtubeSearch.get("/search", {
+          params: {
+            q: `${mood} songs`,
+            maxResults: 15 - songsArray[mood].length,
+          },
+        });
+        setSearchResult(res.data.items);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (songsArray[mood]?.length < 15) {
+      getSearchResults();    
+    }
+  }, [songsArray, mood])
+  
+  useEffect(() => {
+    if (searchResult.length > 0) {
+      setIsLoading(false);
+    }
+  }, [searchResult])
+
+
+  if (areUserSongsLoading) {
+    return CircularLoader();
+  } else if (songsArray[mood]?.length < 15) {
+    return (
+      // get mixed results
+      <div style={{height:'fit-content'}}>
+        {getCustomPlaylist(songsArray[mood])}
+        {isLoading ? CircularLoader() : searchResult.map((song) => {
+          const { snippet } = song;
+          return <FetchPlaylist song={snippet} />
+        })}
+      </div>
+    );
+
+  } else if (songsArray[mood]?.length >= 15) {
+    // get only user songs but EMO customized songs
+    return getCustomPlaylist(songsArray[mood]);
+  }
+  return null;
+}
 
 const mapper = (state) => ({
-  songsArray: state.songs,
+  songsArray: state.song,
+  areUserSongsLoaded: state.song.isLoading,
 });
 
-export default connect(mapper, { userPlaySong, userSkipSong })(
-  withRouter(MoodPlaylist)
-);
+export default connect(mapper, { userPlaySong, userSkipSong })(MoodPlaylist);
