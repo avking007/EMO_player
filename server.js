31d71285054d01de6173da428eeb2ab37ebcd031
Anish <avanvarshney@gmail.com>
2021-07-10 18:22:54 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require("cors")
+const cors = require("cors");
+const path = require('path');
 
 const app = express();
 
@@ -9,11 +10,21 @@ connectDB();
 
 // middleware for handling body request
 app.use(express.json({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 // api routes
 app.use('/user', require('./routes/auth'));
 app.use('/song', require('./routes/songs'));
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.use('*', (req,res)=>{
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
+  });
+}
 
 // define port for server
 const PORT = process.env.PORT || 5000;
