5538d35d9cd683691223c3b82afbacd2584fa625
Anish <avanvarshney@gmail.com>
2021-07-11 01:06:34 +0530

    Removed console log

--- client/src/apis/youtubeSearch.js
+++ client/src/apis/youtubeSearch.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const selectRandomKey = () => {
 
-  console.log(process.env);
   const keys = process.env.REACT_APP_YOUTUBE_KEYS.split(" ");
 
   const random = Math.floor(Math.random() * Math.floor(keys.length)); //this will get a random number
