7c0df16bcf6b7a421f66a8d8abebac06f8ee2ef3
Anish <avanvarshney@gmail.com>
2021-07-11 15:02:16 +0530

    Removed keys

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI || "mongodb+srv://anishv:anish1234@emo.kusoz.mongodb.net/UserDB?retryWrites=true&w=majority",
-  jwtKey: process.env.JWT_KEY || "ptBVTGk12Itfbg5H",
+  mongoURI: process.env.MONGODB_URI,
+  jwtKey: process.env.JWT_KEY,
 };
