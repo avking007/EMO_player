97c7e20cb69120e8788603dc42c78b71e4d3cc0f
Anish <avanvarshney@gmail.com>
2021-08-02 19:29:20 +0530

    added alerts on api fails

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT_SUCCESS, SIGNUP_FAIL, SIGNUP_SUCCESS, USER_LOADED, USER_SONGS_LOADED } from '../reducers/types';
 import setAuthToken from '../utils/setAuthToken';
+import {set_alert} from './alert';
 
 export const loadUser = () => async (dispatch) => {
 
@@ -16,7 +17,10 @@ export const loadUser = () => async (dispatch) => {
         dispatch({ type: USER_LOADED, payload: userData });
         dispatch({type: USER_SONGS_LOADED, payload: user.data.user.songDetails})
     } catch (error) {
-        console.log(error);
+        const err = error.response.data.errors;
+        if (err) {
+          err.forEach((error) => dispatch(set_alert(error.msg, 'danger')));
+        }
         dispatch({ type: AUTH_ERROR });
     }
 }
@@ -34,7 +38,10 @@ export const login = (userDetails) => async (dispatch) => {
         dispatch({ type: LOGIN_SUCCESS, payload: user.data });
         dispatch(loadUser());
     } catch (error) {
-        console.log(error);
+        const err = error.response.data.errors;
+        if (err) {
+          err.forEach((error) => dispatch(set_alert(error.msg, 'danger')));
+        }
         dispatch({ type: LOGIN_FAIL });
     }
 };
@@ -53,8 +60,11 @@ export const signUp = (userDetails) => async (dispatch) => {
         dispatch({ type: SIGNUP_SUCCESS, payload: user.data });
         dispatch(loadUser());
     } catch (error) {
+        const err = error.response.data.errors;
+        if (err) {
+          err.forEach((error) => dispatch(set_alert(error.msg, 'danger')));
+        }
         dispatch({ type: SIGNUP_FAIL });
-        console.log(error);
     }
 };
 
